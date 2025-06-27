bf5d235d465266b5d922d43613f980b6daa9e19b
imdinnesh <itsdinnesh@gmail.com>
2025-06-27 15:57:18 +0530

    add: auth-test route

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -9,8 +9,13 @@ import (
 
 func main() {
 	Logger.Log.Info().Msg("Starting Auth Service")
-	cfg:=config.Load()
+	cfg := config.Load()
 	router := gin.Default()
+	router.GET("/auth-test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Auth Service is running",
+		})
+	})
 	public := router.Group("/api/auth")
 	{
 		public.POST("/register", handler.Register)
@@ -18,7 +23,7 @@ func main() {
 		public.POST("/refresh", handler.RefreshToken)
 	}
 
-	if err := router.Run(":"+cfg.ServerPort); err != nil {
+	if err := router.Run(":" + cfg.ServerPort); err != nil {
 		Logger.Log.Fatal().Err(err).Msg("Failed to start Auth Service")
 	}
 	Logger.Log.Info().Msg("Auth Service started successfully")
