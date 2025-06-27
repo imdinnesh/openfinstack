89f58e08829dd29df55b3801c0452e3f02448017
imdinnesh <itsdinnesh@gmail.com>
2025-06-27 15:47:29 +0530

    add: config file for auth service

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	Logger "github.com/imdinnesh/openfinstack/packages/logger"
+	"github.com/imdinnesh/openfinstack/services/auth/config"
 	"github.com/imdinnesh/openfinstack/services/auth/internal/handler"
 )
 
 func main() {
 	Logger.Log.Info().Msg("Starting Auth Service")
+	cfg:=config.Load()
 	router := gin.Default()
 	public := router.Group("/api/auth")
 	{
-		public.POST("/register",handler.Register)
+		public.POST("/register", handler.Register)
 		public.POST("/login", handler.Login)
 		public.POST("/refresh", handler.RefreshToken)
 	}
 
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(":"+cfg.ServerPort); err != nil {
 		Logger.Log.Fatal().Err(err).Msg("Failed to start Auth Service")
 	}
 	Logger.Log.Info().Msg("Auth Service started successfully")
---
+++ services/auth/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Env        string
+	ServerPort string
+}
+
+func Load() *Config {
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "dev"
+	}
+
+	// Load corresponding .env file
+	if err := godotenv.Load(".env." + env); err != nil {
+		log.Printf("Warning: could not load .env.%s file: %v", env, err)
+	}
+
+	cfg := &Config{
+		Env:        getEnv("ENV", "dev"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+
+	fmt.Println("Loaded environment:", cfg.Env)
+	return cfg
+}
+
+func getEnv(key, defaultVal string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultVal
+	}
+	return val
+}
