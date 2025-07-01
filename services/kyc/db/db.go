84fea7e5c0a7822a8adacee3b0c4511a6c938bdf
imdinnesh <itsdinnesh@gmail.com>
2025-07-01 21:52:36 +0530

    add: repo layer for kyc

---
+++ services/kyc/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/imdinnesh/openfinstack/services/kyc/config"
+	"github.com/imdinnesh/openfinstack/services/kyc/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB(cfg *config.Config) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(cfg.DBUrl), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Auto migrate profile table
+	err = db.AutoMigrate(&models.Profile{})
+	if err != nil {
+		log.Fatal("Failed to run migrations:", err)
+	}
+
+	fmt.Println("Database connected & migrated successfully")
+	return db
+}
