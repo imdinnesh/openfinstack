b004da9152d9b4d0cd49297535d5e54a840f9518
imdinnesh <itsdinnesh@gmail.com>
2025-07-23 20:17:58 +0530

    fix: phone unique issue

--- services/auth/models/model.go
+++ services/auth/models/model.go
@@ -5,9 +5,9 @@ import (
 )
 
 type User struct {
-	ID           uint   `gorm:"primaryKey"`
-	Email        string `gorm:"uniqueIndex;not null"`
-	Phone        string `gorm:"uniqueIndex"`
+	ID           uint    `gorm:"primaryKey"`
+	Email        string  `gorm:"uniqueIndex;not null"`
+	Phone       *string `gorm:"uniqueIndex"`
 	PasswordHash string `gorm:"not null"`
 	IsVerified   bool   `gorm:"default:false"`
 	Role         string `gorm:"default:'user'"`
