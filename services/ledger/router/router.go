de50e3b0fd55872614b542d276a9e8f14f0ed880
imdinnesh <itsdinnesh@gmail.com>
2025-08-22 20:30:37 +0530

    add: implement router for ledger service with test route and API versioning

---
+++ services/ledger/router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/imdinnesh/openfinstack/services/ledger/config"
+	"github.com/imdinnesh/openfinstack/services/ledger/internal/routes"
+	"gorm.io/gorm"
+)
+
+func New(cfg *config.Config, db *gorm.DB) *gin.Engine {
+	router := gin.Default()
+
+	// test route
+	router.GET("/ledger-test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Ledger Service is running",
+		})
+	})
+
+	public := router.Group("/api/v1")
+	routes.RegisterLedgerRoutes(public, db, cfg)
+	return router
+}
