f20e7f60b416e6ffaeaf30a8ee57277e6eb70888
imdinnesh <itsdinnesh@gmail.com>
2025-09-01 19:10:07 +0530

    feat: implement IsExpired method in RefreshTokenRepository for token expiration check

--- services/auth/internal/repository/refreshtoken.repo.go
+++ services/auth/internal/repository/refreshtoken.repo.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/imdinnesh/openfinstack/services/auth/models"
 	"gorm.io/gorm"
 )
@@ -8,6 +10,7 @@ import (
 type RefreshTokenRepository interface {
 	Create(token *models.RefreshToken) error
 	DeleteByUserID(userID uint) error
+	IsExpired(userID uint) (bool, error)
 }
 
 type refreshTokenRepository struct {
@@ -22,7 +25,18 @@ func (r *refreshTokenRepository) Create(token *models.RefreshToken) error {
 	return r.db.Create(token).Error
 }
 
-
 func (r *refreshTokenRepository) DeleteByUserID(userID uint) error {
 	return r.db.Where("user_id = ?", userID).Delete(&models.RefreshToken{}).Error
+}
+
+func (r *refreshTokenRepository) IsExpired(userID uint) (bool, error) {
+	var refreshToken models.RefreshToken
+
+	err := r.db.Where("user_id = ?", userID).First(&refreshToken).Error
+	if err != nil {
+		return false, err
+	}
+
+	// true if expired, false if still valid
+	return refreshToken.ExpiresAt.Before(time.Now()), nil
 }
